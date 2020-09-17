7168c625152f80087c9a80c83fdebdf03b18d0ff
unknown <georgi.nanev@abv.bg>
2020-09-17 14:32:28 +0300

    repositioned ships

--- src/components/Ship/ShipUI.js
+++ src/components/Ship/ShipUI.js
@@ -5,12 +5,13 @@ export const StyledShip = styled.div.attrs(
     style: {
       top: row * 32,
       left: col * 32,
-      width: orientation ? 32 * length + 1 : "33px",
-      height: !orientation ? 32 * length + 1 : "33px",
+      width: orientation ? 31 * length + 1 : "29px",
+      height: !orientation ? 31 * length + 1 : "29px",
     },
   })
 )`
   position: absolute;
   border: 2px solid #00f;
+  border-radius: 2px;
   background-color: rgba(0, 0, 255, 0.05);
 `;
